e31af5eaf20731bd6d33ac2aed137184f4c33a7a
Arvind Parkash <arvindparkash1999@gmail.com>
2024-01-30 20:01:13 +0530

    added jwt authentication

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const User = require("./db/User");
 const Product = require("./db/Product");
 const cors = require("cors");
 const app =  express();
+const JWT = require("jsonwebtoken");
+const JWTkey = 'e-comm';
 
 app.use(express.json());
 app.use(cors());
@@ -17,20 +19,34 @@ app.post('/register', async (req, res) =>{
     let data = await user.save();
     data = data.toObject();
     delete data.password;
-    res.send(JSON.stringify(data));
+    JWT.sign({data}, JWTkey, (err,token)=>{
+        if(err){
+            res.send({status:'Something went wrong, Please try again'})
+        }else{
+            res.send({user:data,auth:token});
+        }
+    });
+    // res.send(JSON.stringify(data));
 });
 app.post("/login", async (req,res) =>{
+    // console.lo(req.body.email);
     if(req.body.email && req.body.password){
         let result = await User.findOne(req.body).select("-password");
         console.log(result);
         if(result){
-            res.send(result);
+            JWT.sign({result}, JWTkey,{expiresIn:"2h"}, (err,token)=>{
+                if(err){
+                    res.send({status:'Something went wrong, Please try again'})
+                }else{
+                    res.send({user:result,auth:token});
+                }
+            });
         }else{
         res.send(JSON.stringify({status:"Invalid email and password"}));
 
         }
     }else{
-        res.send(JSON.stringify({status:"Email And Passwor required"}));
+        res.send(JSON.stringify({status:"Email And Passwor required",body:req.body}));
     }
 });
 
@@ -39,7 +55,7 @@ app.post("/product", async (req, res ) =>{
     product = await product.save();
     res.send(JSON.stringify(product));
 });
-app.get("/product", async (req,resp) =>{
+app.get("/product",verifyToken, async (req,resp) =>{
     let product = await Product.find();
     if(product){
         resp.send(JSON.stringify(product));
@@ -91,6 +107,36 @@ app.delete("/product/:id", async (req,resp ) =>{
         resp.send({status:"Invalid Product Id"});
     }    
 })
+app.get("/search/:key", async (req, rep) =>{
+    let product = await Product.find({
+        "$or":[
+            { productTitle: { $regex:req.params.key } },
+            { category: { $regex:req.params.key } },
+            { companyName: { $regex:req.params.key } },
+        ]
+    });
+    rep.send(product);
+})
 
+function verifyToken(req,res,next){
+    let token = req.headers['authorization'];
+    if(token){
+        token = token.split(" ")[1];
+        if(token){
+            JWT.verify(token,JWTkey,(err,valid)  =>{
+                if(err){
+                    res.send({status:"Invalid JWT token1"});
+                }else{
+                    next();
+                }
+            })
+        }else{
+            res.send({status:"Invalid JWT token"});
+
+        }
+    }else{
+        res.send({status:"Please provide token"});
+    }
+}
 
 app.listen(4500);
